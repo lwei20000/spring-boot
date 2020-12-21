7436bff747d1e62ab508af91914d5e166dc8d5ef
lwei20000 <1291958264@qq.com>
2020-12-21 12:21:30 +0800

    环境搭建

---
+++ my-test/src/main/java/initializer/initializer/FirstInitializer.java
@@ -0,0 +1,53 @@
+package initializer.initializer;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 系统初始化器
+ *
+ * @Auther: weiliang
+ * @Date: 2020/12/21 11:34
+ * @Description:
+ */
+@Order(1)
+public class FirstInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+	@Override
+	public void initialize(ConfigurableApplicationContext applicationContext) {
+		ConfigurableEnvironment environment =  applicationContext.getEnvironment();
+		Map<String, Object> map = new HashMap<>();
+		map.put("key1", "value");
+		MapPropertySource mapPropertySource = new MapPropertySource("firstInitilizer", map);
+		environment.getPropertySources().addLast(mapPropertySource);
+		System.out.println("=====================> run firstInitializer");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ my-test/src/main/java/initializer/initializer/SecondInitializer.java
@@ -0,0 +1,53 @@
+package initializer.initializer;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 系统初始化器
+ *
+ * @Auther: weiliang
+ * @Date: 2020/12/21 11:34
+ * @Description:
+ */
+@Order(2)
+public class SecondInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+	@Override
+	public void initialize(ConfigurableApplicationContext applicationContext) {
+		ConfigurableEnvironment environment =  applicationContext.getEnvironment();
+		Map<String, Object> map = new HashMap<>();
+		map.put("key2", "value");
+		MapPropertySource mapPropertySource = new MapPropertySource("firstInitilizer2", map);
+		environment.getPropertySources().addLast(mapPropertySource);
+		System.out.println("=====================> run firstInitializer2");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
