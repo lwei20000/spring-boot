50ac0e9eacda0bcc0fb439cad5e0bf8799d9dd5b
lwei20000 <1291958264@qq.com>
2020-12-21 10:20:13 +0800

    环境搭建

---
+++ my-test/src/main/java/SBApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: weiliang
+ * @Date: 2020/12/21 10:14
+ * @Description:
+ */
+@SpringBootApplication
+public class SBApplication {
+}
